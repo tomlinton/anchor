9c0c97b4b5f4cefbf36c5d262755d51e519e3a2d
Tom Linton <tlinton@fastmail.fm>
2021-10-09 17:23:01 +1300

    Composite failure example

--- tests/composite/programs/composite/src/lib.rs
+++ tests/composite/programs/composite/src/lib.rs
@@ -13,21 +13,22 @@ mod composite {
     }
 
     pub fn composite_update(
-        ctx: Context<CompositeUpdate>,
-        dummy_a: u64,
-        dummy_b: u64,
+        _ctx: Context<CompositeUpdate>,
+        _dummy_a: u64,
+
+        _dummy_b: u64,
     ) -> ProgramResult {
-        let a = &mut ctx.accounts.foo.dummy_a;
-        let b = &mut ctx.accounts.bar.dummy_b;
-
-        a.data = dummy_a;
-        b.data = dummy_b;
-
         Ok(())
     }
 }
 
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Default)]
+pub struct MyArgs {
+    foo: u16,
+}
+
 #[derive(Accounts)]
+#[instruction(args: MyArgs)]
 pub struct Initialize<'info> {
     #[account(zero)]
     pub dummy_a: Account<'info, DummyA>,
@@ -36,21 +37,9 @@ pub struct Initialize<'info> {
 }
 
 #[derive(Accounts)]
+#[instruction(args: MyArgs)]
 pub struct CompositeUpdate<'info> {
-    foo: Foo<'info>,
-    bar: Bar<'info>,
-}
-
-#[derive(Accounts)]
-pub struct Foo<'info> {
-    #[account(mut)]
-    pub dummy_a: Account<'info, DummyA>,
-}
-
-#[derive(Accounts)]
-pub struct Bar<'info> {
-    #[account(mut)]
-    pub dummy_b: Account<'info, DummyB>,
+    initialize: Initialize<'info>,
 }
 
 #[account]
